5aa7bdbeb7861232b1d2039ea5fed1af9cf0fce6
Donny Gies <Donnygies@gmail.com>
2021-02-27 17:32:46 -0500

    gut it

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,80 @@
-const gulp = require('gulp'),
+const 
+    gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     path = require('path'),
-    builder = require('gulp-nw-builder'),
+    livereload = require('gulp-livereload'),
+    nodemon = require('gulp-nodemon'),
+    plumber = require('gulp-plumber'),
     cleanCSS = require('gulp-clean-css');
 
 sass.compiler = require('node-sass');
 
 
 
-
-
-const combineCss = () => {
+const compileCss = () => {
     const modules = path.resolve(__dirname, 'node_modules')
     let files = [
         modules + '/bulma/css/bulma.css',
         './src/public/css/*.css'
     ]
-    return gulp.src(files)
-        .pipe(concat('style.css'))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./dist/public/css/'));
+    return gulp.src('./src/public/css/sass/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('./src/public/css/'))
+    .pipe(gulp.src(files)
+    .pipe(concat('style.css'))
+    .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+    .pipe(gulp.dest('./src/public/css/'))
+    .pipe(livereload()));
 
-}
-
-const css = () => {
-    return gulp.src('./src/public/css/sass/*.scss').pipe(sass())
-        .pipe(gulp.dest('./src/public/css/'))
 }
 
 const build = () => {
-
     let files = [
         "./src/app/**/*.*",
         "./src/config/**/*.*",
         "./src/public/css/*.css",
         "./src/package.json"
-        ];
+    ];
     return gulp.src(files, {
-        base: "./src/"
-    })
-    .pipe(gulp.src('./app.js'))
-    .pipe(gulp.dest('./dist/'))
-
-
+            base: "./src/"
+        })
+        .pipe(gulp.src('./app.js'))
+        .pipe(gulp.dest('./dist/'))
 }
 
 
-
-exports.sass = gulp.series(css, combineCss);
-
-exports.build = gulp.series(build,css, combineCss);;
-
-exports.peek = () => {
-    gulp.watch('./src/public/css/sass/*.scss', gulp.series(css, combineCss));
+const develop = () => {
+    livereload.listen();
+    nodemon({
+        script: './app.js',
+        ext: 'js coffee handlebars',
+        stdout: false
+    }).on('readable', function () {
+        this.stdout.on('data', (chunk) => {
+            if (/^Express server listening on port/.test(chunk)) {
+                livereload.changed(__dirname);
+            }
+        });
+        this.stdout.pipe(process.stdout);
+        this.stderr.pipe(process.stderr);
+    });
 }
+
+
+const peek = () => {
+    gulp.watch('./src/public/css/sass/style.scss', gulp.series(css, combineCss))
+}
+
+
+exports.dev = gulp.series(compileCss,develop,peek);
+exports.sass = gulp.series(compileCss);
+exports.prod = gulp.series(compileCss,build)
+
+
+
+
+exports.peek = peek;
